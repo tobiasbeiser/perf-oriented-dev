7d3162b7e267af1acd46b07ac86367814cdb95d6
Tobias Beiser <tobias.beiser@gmail.com>
2024-05-31 15:34:31 +0200

    Removed DataStructureBenchmark folder as it is now a submodule

--- tools/DataStructureBenchmark/ArrayBenchmark.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Benchmark.h"
-template<typename T>
-class ArrayBenchmark : public Benchmark<T>
-{
-public:
-	ArrayBenchmark(int runtime, int collectionSize, int readPercentage, int insertPercentage) : Benchmark<T>(runtime, collectionSize, readPercentage, insertPercentage) {}
-	void runBenchmark() override;
-};
